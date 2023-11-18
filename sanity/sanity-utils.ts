fbace2a8f6482f66a1c34ecae08eeb6ef2317e5a
Daria Kliukhina <dariasergeevna90@gmail.com>
2023-11-17 23:44:29 -0800

    fix error message, add getData types

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,10 +1,10 @@
 import { client } from "./lib/client";
 
-export async function getData(query, params) {
+export async function getData(query: string, params: object) {
   try {
     return client.fetch(query, params);
   } catch(error) {
-    console.error("Error fetching homepage data:", error);
+    console.error("Error fetching data:", error);
     return {error: "Error occurred"};
   }
 }
