cf45900fdd16fa6e0cd7327a95a87da473a277d9
Daria Kliukhina <dariasergeevna90@gmail.com>
2023-12-26 20:59:17 -0800

    selete zustand

--- src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand'
-import { StoreState } from './types'
-
-export const useStore = create<StoreState>((set) => ({
-  language: 'en',
-  setLanguage: () => set((state: any) => ({ language: state })),
-}))
--- src/store/types/index.ts
+++
@@ -1,6 +0,0 @@
-export type LanguageProps = 'en' | 'ru';
-
-export interface StoreState {
-  language: LanguageProps;
-  setLanguage: (state: LanguageProps) => void;
-}
