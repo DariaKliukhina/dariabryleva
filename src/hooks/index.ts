034c015268f3e0242150fe3f6fa60bf694618cf0
Daria Kliukhina <dariasergeevna90@gmail.com>
2024-01-15 17:57:34 -0800

    refactoring

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,15 +1,23 @@
-import { usePathname, useParams } from "next/navigation";
+import { DEFAULT_LOCALE, LOCALES } from "@/utils";
+import { usePathname, useParams, redirect } from "next/navigation";
 
 export const useLocales = () => {
   const pathname = usePathname();
   const params = useParams();
-  const currentLocale = params.locale || "en";
+
+  const currentLocale = LOCALES.includes(params.locale as string) ? params.locale : null;
   const targetLocale = currentLocale === "ru" ? "en" : "ru";
   const targetPath = pathname.replace(/^\/(en|ru)/, "/" + targetLocale)
+  const redirectPath = DEFAULT_LOCALE + '/' + pathname
+
+  function redirectToDefaultLocale() {
+    redirect(redirectPath)
+  }
   
   return {
     currentLocale,
     targetLocale,
-    targetPath
+    targetPath,
+    redirectToDefaultLocale
   }
 }
