c4da3e03ac1839d97cc73e55e0ab406e7ac011cb
Daria Kliukhina <dariasergeevna90@gmail.com>
2024-03-31 21:46:13 -0700

    add position markup and types

--- src/components/Blocks/Position/index.tsx
+++ src/components/Blocks/Position/index.tsx
@@ -1,5 +1,25 @@
-import { Box } from "@mantine/core";
-import React from "react";
+import { Box, Flex } from "@mantine/core";
+import Link from "next/link";
+import classes from "./Position.module.css";
+
+type TechnologiesProps = {
+  slug: string;
+  title: string;
+};
+
+type CompanyProps = {
+  companyName: string;
+  link: string;
+};
+
+type PositionProps = {
+  company: CompanyProps;
+  description: string;
+  position: string;
+  locationType: string;
+  workType: string;
+  technologies: TechnologiesProps[];
+};
 
 export const Position = ({
   company,
@@ -8,13 +28,26 @@ export const Position = ({
   workType,
   position,
   technologies,
-}) => {
-  return <Box>
-    {company.name}
-    {position}
-    {workType}
-    {locationType}
-    {description}
-    {technologies.map((item) => <Box key={item.slug}>{item.title}</Box>)}
-  </Box>;
+}: PositionProps) => {
+  return (
+    <Box>
+      <Flex justify="space-between" className={classes.header}>
+        <Box>
+          <Link href={company.link}>{company.companyName}</Link>
+          <Box>{position}</Box>
+          <Box>{locationType}</Box>
+        </Box>
+
+        <Box>{workType}</Box>
+      </Flex>
+
+      <Box className={classes.description}>{description}</Box>
+
+      <Box className={classes.technologies}>
+        {technologies.map((item) => (
+          <Box key={item.slug}>{item.title}</Box>
+        ))}
+      </Box>
+    </Box>
+  );
 };
--- src/components/Sections/ExperienceList/index.tsx
+++ src/components/Sections/ExperienceList/index.tsx
@@ -1,10 +1,19 @@
+import { Box } from "@mantine/core";
 import { Position } from "../../Blocks/Position";
-import React from "react";
+import classes from "./ExperienceList.module.css";
 
 export const ExperienceList = ({ list }) => {
+  console.log(list);
   return (
     <div>
-      {list?.map(item => <Position key={item.company.companyName + item.position} {...item}/>)}
+      {list?.map((item) => (
+        <Box
+          key={item.company.companyName + item.position}
+          className={classes.position}
+        >
+          <Position {...item} />
+        </Box>
+      ))}
     </div>
   );
 };
