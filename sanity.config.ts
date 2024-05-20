4d33999a45138652bc497615415159a62bc75979
Daria Kliukhina <dariasergeevna90@gmail.com>
2024-05-19 22:35:37 -0700

    upgrade sanity structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,14 @@
 /**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...index]]/page.tsx` route
  */
 
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import { structure } from "./sanity/structure";
+import { structureTool } from "sanity/structure";
+import {structure} from "./sanity/structure"
 import { I18nFields } from "sanity-plugin-i18n-fields";
 import { media } from "sanity-plugin-media";
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schema";
 
@@ -17,7 +16,6 @@ export default defineConfig({
   basePath: "/admin",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
     I18nFields({
@@ -30,7 +28,7 @@ export default defineConfig({
         { code: "ru", label: "🇷🇺", title: "Russian" },
       ],
     }),
-    deskTool({ structure }),
+    structureTool({ structure }),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
