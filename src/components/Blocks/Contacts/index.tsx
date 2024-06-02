116add60b627f8caccbc82238ad6fb8793b16281
Daria Kliukhina <dariasergeevna90@gmail.com>
2024-06-02 15:27:28 -0700

    improve form

--- src/components/Blocks/Contacts/index.tsx
+++ src/components/Blocks/Contacts/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  Box,
   Button,
   Center,
   Checkbox,
@@ -12,6 +13,7 @@ import {
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import classes from "./Contacts.module.css";
+import { useState } from "react";
 
 type ContactsProps = {
   name: string;
@@ -38,6 +40,7 @@ export const Contacts = ({
   messageError,
   submit,
 }: ContactsProps) => {
+  const [isLoading, setIsLoading] = useState(false);
   const form = useForm({
     initialValues: {
       email: "",
@@ -54,56 +57,67 @@ export const Contacts = ({
   });
 
   async function handleSubmit(data) {
+    setIsLoading(true);
     const json = JSON.stringify(data);
-    console.log(json);
 
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: json,
-    });
-    const result = await response.json();
-    if (result.success) {
-      console.log('AAAAA', result);
-    } else {
-      
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json,
+      });
+      const result = await response.json();
+      if (result.success) {
+        form.reset();
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
-    <form onSubmit={form.onSubmit(handleSubmit)}>
-      <Stack gap={rem(4)}>
-        <Flex gap={rem(14)} direction={{ base: "column", xs: "row" }}>
-          <TextInput
-            withAsterisk
-            label={email}
-            placeholder={emailPlaceholder}
-            className={classes.input}
-            {...form.getInputProps("email")}
-          />
-          <TextInput
-            withAsterisk
-            label={name}
-            placeholder={namePlaceholder}
-            className={classes.input}
-            {...form.getInputProps("name")}
-          />
-        </Flex>
+    <Box
+      style={{
+        opacity: isLoading ? 0.5 : 1,
+        pointerEvents: isLoading ? "none" : "initial",
+      }}
+    >
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <Stack gap={rem(4)}>
+          <Flex gap={rem(14)} direction={{ base: "column", xs: "row" }}>
+            <TextInput
+              withAsterisk
+              label={email}
+              placeholder={emailPlaceholder}
+              className={classes.input}
+              {...form.getInputProps("email")}
+            />
+            <TextInput
+              withAsterisk
+              label={name}
+              placeholder={namePlaceholder}
+              className={classes.input}
+              {...form.getInputProps("name")}
+            />
+          </Flex>
 
-        <Textarea
-          withAsterisk
-          label={message}
-          placeholder={messagePlaceholder}
-          className={classes.input}
-          {...form.getInputProps("message")}
-        />
-      </Stack>
+          <Textarea
+            withAsterisk
+            label={message}
+            placeholder={messagePlaceholder}
+            className={classes.input}
+            {...form.getInputProps("message")}
+          />
+        </Stack>
 
-      <Center mt="md">
-        <Button type="submit">{submit}</Button>
-      </Center>
-    </form>
+        <Center mt="md">
+          <Button type="submit">{submit}</Button>
+        </Center>
+      </form>
+    </Box>
   );
 };
