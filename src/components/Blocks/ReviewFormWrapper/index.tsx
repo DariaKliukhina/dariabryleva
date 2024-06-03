f59c281558efeeb285b126953f255d079b3412d9
Daria Kliukhina <dariasergeevna90@gmail.com>
2024-06-02 19:43:06 -0700

    add a leave-review page

---
+++ src/components/Blocks/ReviewFormWrapper/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState } from "react";
+import { ReviewForm } from "../ReviewForm";
+import { Box, Center, Title, rem } from "@mantine/core";
+import classes from "./ReviewFormWrapper.module.css";
+
+type ReviewFormWrapperProps = {
+  data: {
+    name: string;
+    namePlaceholder: string;
+    nameError: string;
+    email: string;
+    emailPlaceholder: string;
+    message: string;
+    messagePlaceholder: string;
+    messageError: string;
+    submit: string;
+    title: string;
+    success: string;
+    howDoYouKnowMe: string;
+    teacher: string;
+    collegue: string;
+  };
+};
+
+export const ReviewFormWrapper = ({ data }: ReviewFormWrapperProps) => {
+  const [isSent, setIsSent] = useState(false);
+
+  function onSuccess() {
+    setIsSent(true);
+  }
+
+  return (
+    <Center>
+      <Box className={classes.root}>
+        <Title size={rem(38)} className={classes.title}>
+          {isSent ? data.success : data.title}
+        </Title>
+        {!isSent && <ReviewForm {...data} onSuccess={onSuccess} />}
+      </Box>
+    </Center>
+  );
+};
